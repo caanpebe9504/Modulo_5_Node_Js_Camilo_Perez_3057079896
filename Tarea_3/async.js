9d0cd391dc824017bf72bbf9940f23a19d269dcb
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-11-06 18:29:51 -0500

    Tarea_3 finalizada

--- Tarea_3/async.js
+++ Tarea_3/async.js
@@ -3,9 +3,9 @@ const esperarSegundos = (segundos) => {
     
     return new Promise((resolve) => {
       setTimeout(() => {
-        console.log(`Han pasado ${segundos} segundos.`); // Mostrar mensaje
-        resolve(); // Resolver la promesa
-      }, segundos * 1000); // Multiplicar por 1000 para convertir segundos en milisegundos
+        console.log(`Han pasado ${segundos} segundos.`); 
+        resolve(); 
+      }, segundos * 1000); 
     });
 }
 
